c6d65b634b4d9adeb9f64f167a5134a9ed1194b8
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2022-03-20 23:10:48 -0700

    Started working on the submission for part 1
    I got the map made and entered in the game and began working on the health part for the game

---
+++ Project04_Tower_Defense/Assets/Scripts/EnemyDemo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDemo : MonoBehaviour
+{
+    // todo #1 set up properties
+    //   health, speed, coin worth
+
+    public int health = 3;
+    public float speed = 3f;
+    public int coins = 3;
+
+    //   waypoints
+
+    public List<Transform> waypoints;
+    //   delegate event for outside code to subscribe and be notified of enemy death
+
+    // NOTE! This code should work for any speed value (large or small)
+
+    //-----------------------------------------------------------------------------
+    void Start()
+    {
+        // todo #2
+        //   Place our enemy at the starting waypoint
+    }
+
+    //-----------------------------------------------------------------------------
+    void Update()
+    {
+        // todo #3 Move towards the next waypoint
+        // todo #4 Check if destination reaches or passed and change target
+    }
+
+    //-----------------------------------------------------------------------------
+    private void TargetNextWaypoint()
+    {
+    }
+}
